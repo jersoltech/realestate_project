97d0f786a1d3fd0fef748941696459376c9695b7
Jeremiah Solomon <jeremiahsolomonfrancis@gmail.com>
2020-08-03 12:50:29 +0100

    adding listing and realtors app

--- realestate/urls.py
+++ realestate/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('pages.url')),
+    path('listings/', include('listings.url')),
     path('admin/', admin.site.urls),
 ]
