c2836d3cdd1bc90df403842063a111f43d06ed84
Jeremiah Solomon <jeremiahsolomonfrancis@gmail.com>
2020-08-08 03:45:22 +0100

    creating of models and database migration

--- realestate/settings.py
+++ realestate/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'realestate.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'realestate',
+        'USER': 'postgres',
+        'PASSWORD': 'user',
+        'HOST': 'localhost'
     }
 }
 
@@ -125,3 +128,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'realestate/static')
 ]
+
+#Media Folder Paths
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+
--- realestate/urls.py
+++ realestate/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('pages.url')),
     path('listings/', include('listings.url')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
